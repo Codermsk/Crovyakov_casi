0cef60e814c73de045664022b27e7992762c7b29
Maksim Vladimirov <maksion.com@gmail.com>
2025-04-07 22:38:47 +0300

    Update settings.lua

--- сonfig/settings.lua
+++ сonfig/settings.lua
@@ -1,15 +1,25 @@
 local settings = {}
 
-settings.REPOSITORY = "https://raw.githubusercontent.com/krovyaka/OpenComputers-Casino/master"
-settings.TITLE = "Приветствуем ваc у нас в казино"
-settings.ADMINS = { "krovyak", "Durex77" }
+settings.REPOSITORY = "https://github.com/Codermsk/Crovyakov_casi/tree/master"
+settings.TITLE = "Приветствуем вас в нашем казино"
+settings.ADMINS = { "CoderMS"}
 
--- CHEST - Взаимодействие сундука и МЕ сети
--- PIM - Взаимодействие PIM и МЕ сети
--- CRYSTAL - Взаимодействие кристального сундука и алмазного сундука
--- DEV - Оплата не взимается, награда не выдается, не требует внешних компонентов
-settings.PAYMENT_METHOD = "PIM"
-settings.CONTAINER_PAY = "DOWN"
-settings.CONTAINER_GAIN = "UP"
+-- Доступные методы оплаты:
+-- CHEST    - Взаимодействие с сундуком и ME сетью
+-- PIM      - Взаимодействие с PIM и ME сетью
+-- CRYSTAL  - Взаимодействие кристального сундука и алмазного сундука
+-- TRANSPOSER - Взаимодействие через компонент transposer
+-- DEV      - Режим разработки (без реальных платежей)
+settings.PAYMENT_METHOD = "TRANSPOSER"
 
-return settings;
+-- Настройки для transposer:
+-- PLAYER_SIDE - сторона, где находится инвентарь игрока (обычно DOWN)
+-- SYSTEM_SIDE - сторона системного инвентаря (обычно UP)
+settings.PLAYER_SIDE = "UP"  -- Сторона инвентаря игрока
+settings.SYSTEM_SIDE = "DOWN"    -- Сторона системного инвентаря
+
+-- Старые настройки (оставлены для совместимости, но не используются при PAYMENT_METHOD = TRANSPOSER)
+settings.CONTAINER_PAY = "DOWN"  -- Не используется для transposer
+settings.CONTAINER_GAIN = "UP"   -- Не используется для transposer
+
+return settings
