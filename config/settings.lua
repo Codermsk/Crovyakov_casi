0b5b1816da74f4ff6ed0cfe93341b897af0d4b5a
Maksim Vladimirov <maksion.com@gmail.com>
2025-04-07 23:06:35 +0300

    Create settings.lua

---
+++ config/settings.lua
@@ -0,0 +1,25 @@
+local settings = {}
+
+settings.REPOSITORY = "https://raw.githubusercontent.com/Codermsk/Crovyakov_casi/master"
+settings.TITLE = "Приветствуем вас в нашем казино"
+settings.ADMINS = { "CoderMS"}
+
+-- Доступные методы оплаты:
+-- CHEST    - Взаимодействие с сундуком и ME сетью
+-- PIM      - Взаимодействие с PIM и ME сетью
+-- CRYSTAL  - Взаимодействие кристального сундука и алмазного сундука
+-- TRANSPOSER - Взаимодействие через компонент transposer
+-- DEV      - Режим разработки (без реальных платежей)
+settings.PAYMENT_METHOD = "TRANSPOSER"
+
+-- Настройки для transposer:
+-- PLAYER_SIDE - сторона, где находится инвентарь игрока (обычно DOWN)
+-- SYSTEM_SIDE - сторона системного инвентаря (обычно UP)
+settings.PLAYER_SIDE = "UP"  -- Сторона инвентаря игрока
+settings.SYSTEM_SIDE = "DOWN"    -- Сторона системного инвентаря
+
+-- Старые настройки (оставлены для совместимости, но не используются при PAYMENT_METHOD = TRANSPOSER)
+settings.CONTAINER_PAY = "DOWN"  -- Не используется для transposer
+settings.CONTAINER_GAIN = "UP"   -- Не используется для transposer
+
+return settings
